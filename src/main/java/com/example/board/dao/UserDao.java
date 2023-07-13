dc8d1eca859aff44fb69f07d3fd96df28eea63c1
Gwak-daeun <bbibibbibi22@gmail.com>
2023-07-13 19:43:39 +0900

    스프링 service와 dao부분 진행중

--- src/main/java/com/example/board/dao/UserDao.java
+++ src/main/java/com/example/board/dao/UserDao.java
@@ -1,24 +1,70 @@
 package com.example.board.dao;
 
 import com.example.board.dto.User;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsertOperations;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
 
 @Repository // @Repository는 @Component를 상속받고 있어서 스프링이 관리하는 Bean이다.
 public class UserDao {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate; //jdbcTemplate을 사용하기 위한 변수
+    //final 변수는 생성자에서 값을 초기화 해야 한다.
+
+    private SimpleJdbcInsertOperations insertUser; // sql의 insert를 하게 해주는 인터페이스
+
+    //jdbcTemplate을 초기화 하려면 DataSource가 필요하다.
+    public UserDao(DataSource dataSource) {
+    //application.yml에 db접속 관련 설정을 해주면 DataSource의 Hikari라 불리는 DataSource를 구현하고 있는 객체가 생성된다.
+    //=> 해당 객체를 생성자에 넣어주면 스프링이 자동으로 빈을 생성하면서 final 변수에 넣어준다.
+        jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        insertUser = new SimpleJdbcInsert(dataSource)
+                .withTableName("user")
+                .usingGeneratedKeyColumns("user_id"); // 자동으로 증가되는 id를 설정
+
+    }
+
+    //Spring JDBC를 이용한 코드를 쓸 자리
+
+    @Transactional // 하나의 트랜잭션으로 처리되기 위해
     public User addUser(String email, String name, String password){
-    // insert into user(email, name, password, regdate) values(?, ?, ?, now());
+    //Service에서 이미 트랜잭션이 시작했기 때문에 그 트랜잭션에 포함된다.
+    // insert into user(email, name, password, regdate) values(:email, :name, :password, :regDate); #user_id auto gen
+    // SELECT LAST_INSERT_ID();
 
-        return null;
+        User user = new User();
+        user.setName(name); // name 칼럼
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRegDate(new java.util.Date().toString());//Date 객체를 생성하고 문자열로 변환해서 저장
+        SqlParameterSource params = new BeanPropertySqlParameterSource(user);
+        //BeanPropertySqlParameterSource: DTO에 있는 값을 자동으로 sql parameter source로 넣어주는 객체.
+        // 인터페이스인 SqlParameterSource를 구현하는 객체
+
+       Number number = insertUser.executeAndReturnKey(params); // insert를 실행하고, 자동으로 생성된 id를 가져온다.
+        int userId = number.intValue();
+        user.setUserId(userId);
+
+
+        return user; // insert된 전체 데이터를 user 객체가 가지고 리턴한다.
     }
 
-    public int getLastInsertId() {
-        // SELECT LAST_INSERT_ID();
-        return 0;
-    }
-
+    @Transactional
     public void mappingUserRole(int userId) {
+    //Service에서 이미 트랜잭션이 시작했기 때문에 그 트랜잭션에 포함된다.
     //insert into user_role(user_id, role_id) values(? 1);
 
+        String sql = "insert into user_role(user_id, role_id) values(:userId, 1)";
+        SqlParameterSource params = new MapSqlParameterSource("userId", userId);
+        //MapSqlParameterSource의 인자로 parameter name과 value를 넣을 수도 있고, 여러개일 땐 Map자체를 넣어줄 수도 있다.
+        jdbcTemplate.update(sql, params);
     }
 }
  /*
--- src/main/java/com/example/board/service/UserService.java
+++ src/main/java/com/example/board/service/UserService.java
@@ -3,30 +3,37 @@ package com.example.board.service;
 
 import com.example.board.dao.UserDao;
 import com.example.board.dto.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 //트랜잭션 단위로 실행될 메소드를 선언하고 있는 클래스
 @Service // @Service도 @Component를 상속받고 있어서 스프링이 관리하는 Bean이다.
+@RequiredArgsConstructor//lombok이 final한 변수를 초기화해주는 생성자를 쉽게 자동으로 만들어준다.
 public class UserService {
 
     private final UserDao userDao; // 이렇게 인젝션 되는 dao는 final로 선언하는게 좋다.
-    //final로 선언된 것은 반드시 생성자 주입을 해야 한다.
+    //final로 선언된 것은 반드시 생성자를 통해 초기화가 돼야 한다.
+
+    //만약, final 변수가 여러개라면 생성자에서 final 변수를 인자로 변수 수 만큼 넣어서 초기화 해줘야함
 
     //Spring이 UserService를 Bean으로 생성할 때 생성자를 이용해 생성을 하는데,
     //이때 UserDao Bean이 있는지 보고 그 빈을 주입한다. 생성자 주입
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
-    }
+//    public UserService(UserDao userDao) {
+//        this.userDao = userDao;
+//        //userDao가 들어와서 final 변수가 초기화 된다.
+//    }
 
-    public User addUser(String name, String email, String password) {
-
-
-
-        User user = new User();
-
-
+    //이러한 매소드가 하나의 트랜잭션으로 동작한다.
+    //보통 서비스에서는 @Transactional을 붙여서 하나의 트랜잭션으로 처리하게 한다.
+    //Spring boot는 트랜잭션을 처리해주는 트랜잭션 관리자를 가지고 있다.
+    @Transactional // 트랜잭션으로 동작하기 위한 어노테이션
+    public User addUser(String name, String email, String password) { // 트랜잭션이 시작된다.
+        User user = userDao.addUser(email, name, password);
+        userDao.mappingUserRole(user.getUserId()); // 권한 부여
 
         return user;
+        //리턴 후 트랜잭션이 끝난다.
     }
 }
 /*
